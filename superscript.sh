1ba0542c8acd7bec6bf924d4acaa1a87bd7a770e
JeremySMorgan <jsmorgan6@gmail.com>
2021-02-04 18:54:50 -1000

    removed superscript

--- superscript.sh
+++
@@ -1,6 +0,0 @@
-#/home/pi/Desktop/led_interface/ngrok start --config=/home/pi/.ngrok2/ngrok.yml led_interface & 
-#python ~/Desktop/led_interface/app.py 
-/home/pi/Desktop/led_interface/ngrok start --config=/home/pi/.ngrok2/ngrok.yml led_interface &
-python ~/Desktop/led_interface/app.py 
-
-# sudo /etc/init.d/led_matrix_superscript start
