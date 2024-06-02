ad525ec649b23be1bd3c082fddad8053170d1cd3
jeremy morgan <jsmorgan6@gmail.com>
2024-06-02 07:41:00 -0700

    added indicate_alive_thread()

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from threading import Thread
 from time import time, sleep
 import json
+from datetime import datetime
+import os
 
 import socketio
 
@@ -19,6 +21,10 @@ BRIGHTNESS = 10
 CLEAR_TIME_SECS = 15.0*60.0 # Clear led matrix 15 minutes after a new design is received
 # CLEAR_TIME_SECS = 10
 
+# TODO: there is an issue where the server doesn't reconnect when the internet goes down, or something like that. should 
+# always check to see if a connection is maintained, and try to reconnect if not. 
+# TODO also: - show a green square when connected, red square when disconnected
+# TODO: quit if another app.py process is running
 
 if not SIM_MODE:
     led_writer = LedWriter(BRIGHTNESS)
@@ -56,6 +62,26 @@ def clear_led_thread(delay: float):
     else:
         print("clear_led_thread(): new request recieved during sleeping period, exiting with noops performed")
 
+
+def indicate_alive_thread():
+    dir_path = '/home/jm/Desktop/led-matrix-app/is_alive'
+    prefix = 'iaa' # 'is alive at'
+    print(f"[INFO] starting indicate_alive_thread() to indicate liveliness")
+
+    while True:
+        # delete all directories with prefix
+        dirs = [f for f in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, f))]
+        for subdir in dirs:
+            if subdir[0:len(prefix)] == prefix:
+                # rmdir is safe in that it won't delete a non empty directory - an error if dir contains files 
+                # 'OSError: [Errno 39] Directory not empty'
+                os.rmdir(os.path.join(dir_path, subdir))
+        now_str = datetime.now().strftime('%d:%m:%Y__%H:%M:%S')
+        new_dir = os.path.join(dir_path, f'{prefix}__{now_str}/')
+        os.mkdir(new_dir)
+        sleep(1)
+
+
 @sio.event
 def connect():
     print("[INFO] connected")
@@ -92,6 +118,11 @@ source /home/jm/Desktop/led-matrix-app/venv/bin/activate && python3 /home/jm/Des
 """
 
 if __name__ == "__main__":
+
+    print(f"\n[INFO] started server at {datetime.now()}")
+    show_is_alive_thread = Thread(target=indicate_alive_thread)
+    show_is_alive_thread.start()
+
     clk_thread = Thread(target=clock_thread)
     clk_thread.start()
 
